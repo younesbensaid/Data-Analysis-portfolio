cfad4648c051e212cd7f152054bc34120d8fe596
Younes Bensaid <younesbensa3id@gmail.com>
2026-02-19 23:50:07 +0100

    commit

---
+++ Analysis of Some Data Scientist Jobs Data/Data Analysis & Business Intelligence Roles Tracking.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------------------------------------------
+-- tracking and analyzing job roles related to data analysis, business 
+-- intelligence (BI), and analytics. It includes positions such as Data 
+-- Analysts, Analytics Engineers, and BI Developers. 
+
+
+--------------------------------------------------------------------------------------------
